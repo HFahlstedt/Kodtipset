6675471a54a2a354434f351a5fcb94ac6caa36e2
Håkan Fahlstedt <hf@fahlsoft.se>
2018-02-26 00:40:55 +0100

    Added category content item component

---
+++ src/components/category-content-item.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    td {{ item.title }}
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { ContentData } from "../entities/content-data";
+
+@Component
+export default class CategoryContentItem extends Vue  {
+    @Prop({default: null}) item: ContentData | null; 
+}
+</script>
--- src/components/category-content.vue
+++ src/components/category-content.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
-    .category-content
-        h2 {{ category == undefined ? '' : category.name }}
-        table(v-if='category')
-            tr(v-for='row in category.content' :key='row.id')
-                td {{ row.title }}
+    .category-content-container
+        .category-content
+            h2 {{ category == undefined ? '' : category.name }}
+            table(v-if='category')
+                tr(v-for='row in category.content' :key='row.id')
+                    category-content-item(:item="row")
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import Component from 'vue-class-component';
-import { Prop, Watch } from 'vue-property-decorator';
+import { Prop, Watch, Vue } from 'vue-property-decorator';
 import { ContentData } from '../entities/content-data';
 import { Api } from "../services/api";
 import { Category } from '../entities/category';
-import { RawLocation, Route } from "vue-router/types/router";
+import CategoryContentItem from './category-content-item.vue';
 
-@Component
+@Component({ components: { CategoryContentItem }})
 export default class CategoryContent extends Vue  {
     category: Category | null = null;
     api: Api = new Api();
@@ -37,3 +37,28 @@ export default class CategoryContent extends Vue  {
     }
 }
 </script>
+
+<style lang="scss">
+    .category-content-container {
+        text-align: center;
+
+        .category-content {
+            text-align: left;
+            margin-left: 45px;
+            margin-right: 45px;
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+
+                tr:nth-child(odd) {
+                    background-color: #aaaaaa;
+                }
+
+                tr:nth-child(even) {
+                    background-color: #dddddd;
+                }
+            }
+        }
+    }
+</style>
